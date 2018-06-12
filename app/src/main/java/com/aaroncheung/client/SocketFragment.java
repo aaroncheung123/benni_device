8268781296e30e83509865638bed7b3f95da278e
Aaron Cheung <aaroncheung123@gmail.com>
2018-06-11 18:36:10 -0600

    Added socket fragment

---
+++ app/src/main/java/com/aaroncheung/client/SocketFragment.java
@@ -0,0 +1,84 @@
+package com.aaroncheung.client;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
+
+public class SocketFragment extends Fragment {
+
+
+    String TAG = "debug_123";
+    private String socket_url = "http://192.168.1.144:3000";
+
+    private Socket socket;
+    {
+        Log.d(TAG, "mSocket");
+        try {
+            socket = IO.socket(socket_url);
+        } catch (URISyntaxException e) {}
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.socket_fragment, container, false);
+
+
+        socket.connect();
+        socket.on("message", handleIncomingMessages);
+
+        Button testClick = (Button) view.findViewById(R.id.testButton);
+        testClick.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                attemptSend();
+            }});
+
+        return view;
+    }
+
+    private Emitter.Listener handleIncomingMessages = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, args[0].toString());
+                }
+            });
+        }
+    };
+
+
+    private void attemptSend() {
+        String message = "Hello World";
+        if (TextUtils.isEmpty(message)) {
+            return;
+        }
+        Log.d(TAG, "attempt send");
+        socket.emit("message", message);
+    }
+
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        socket.disconnect();
+    }
+}
